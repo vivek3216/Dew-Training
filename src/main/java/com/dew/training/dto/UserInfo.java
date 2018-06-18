4fb4d09efc85dc4373c299d4397b5bd6e03896a3
Sanya Taneja <Sanya Taneja@DESKTOP-G9D5JP0>
2018-06-18 17:04:06 +0530

    training/dto - updated UserInfo (inserted getter setter for Update Page fields)

--- src/main/java/com/dew/training/dto/UserInfo.java
+++ src/main/java/com/dew/training/dto/UserInfo.java
@@ -1,10 +1,108 @@
 package com.dew.training.dto;
 
+import com.google.api.client.util.DateTime;
+
 public class UserInfo {
 	
-	private String aadharNo;
-	private String panNo;
-	private String passportNo;
+	private String specialization;
+	private DateTime yearOfPassing;
+	private String gradeSystem;
+	private Integer marks;
+	private String college;
+	private String boardtw;
+	private Integer markstw;
+	private String yeartw;
+	private String mediumtw;
+	private String boardte;
+	private Integer markste;
+	private String yearte;
+	private String mediumte;
+	
+	private String ugCourse;
+	public String getUgCourse() {
+		return ugCourse;
+	}
+	public void setUgCourse(String ugCourse) {
+		this.ugCourse = ugCourse;
+	}
+	public String getSpecialization() {
+		return specialization;
+	}
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+	public DateTime getYearOfPassing() {
+		return yearOfPassing;
+	}
+	public void setYearOfPassing(DateTime yearOfPassing) {
+		this.yearOfPassing = yearOfPassing;
+	}
+	public String getGradeSystem() {
+		return gradeSystem;
+	}
+	public void setGradeSystem(String gradeSystem) {
+		this.gradeSystem = gradeSystem;
+	}
+	public Integer getMarks() {
+		return marks;
+	}
+	public void setMarks(Integer marks) {
+		this.marks = marks;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public String getBoardtw() {
+		return boardtw;
+	}
+	public void setBoardtw(String boardtw) {
+		this.boardtw = boardtw;
+	}
+	public Integer getMarkstw() {
+		return markstw;
+	}
+	public void setMarkstw(Integer markstw) {
+		this.markstw = markstw;
+	}
+	public String getYeartw() {
+		return yeartw;
+	}
+	public void setYeartw(String yeartw) {
+		this.yeartw = yeartw;
+	}
+	public String getMediumtw() {
+		return mediumtw;
+	}
+	public void setMediumtw(String mediumtw) {
+		this.mediumtw = mediumtw;
+	}
+	public String getBoardte() {
+		return boardte;
+	}
+	public void setBoardte(String boardte) {
+		this.boardte = boardte;
+	}
+	public Integer getMarkste() {
+		return markste;
+	}
+	public void setMarkste(Integer markste) {
+		this.markste = markste;
+	}
+	public String getYearte() {
+		return yearte;
+	}
+	public void setYearte(String yearte) {
+		this.yearte = yearte;
+	}
+	public String getMediumte() {
+		return mediumte;
+	}
+	public void setMediumte(String mediumte) {
+		this.mediumte = mediumte;
+	}
 	
 
 }
